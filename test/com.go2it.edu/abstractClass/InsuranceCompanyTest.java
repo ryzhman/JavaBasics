46baa1d0a914615160de84040717d73c0d45a698
Oleksandr Ryzhkov <oryzhkov@paymentus.com>
2019-08-17 18:55:51 -0400

    Unit-tests examples

---
+++ test/com.go2it.edu/abstractClass/InsuranceCompanyTest.java
@@ -0,0 +1,47 @@
+package com.go2it.edu.abstractClass;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Alex Ryzhkov
+ */
+public class InsuranceCompanyTest {
+
+	@Test
+	@DisplayName("Should crash since it is created for test purpose")
+	public void testExample1() {
+		assertEquals(3, 1+123, "The sum wasn't calculated properly");
+	}
+
+	@Test
+	public void testExample2() {
+		assertEquals(3, 1+2);
+	}
+
+	@Test
+	public void testCoverClientLoses() {
+		InsuranceCompany testCOmpany = new InsuranceCompany();
+		testCOmpany.setSumToOperate(105);
+
+		double sumReturnedToTheCustomer = testCOmpany.coverClientLoses("Joe Doe", 1000);
+
+		assertEquals(5, sumReturnedToTheCustomer);
+		assertEquals(100, testCOmpany.getSumToOperate());
+	}
+
+	@Test
+	public void testCoverClientLoses2() {
+		InsuranceCompany testCOmpany = new InsuranceCompany();
+		testCOmpany.setSumToOperate(5000);
+
+		double sumReturnedToTheCustomer = testCOmpany.coverClientLoses("Joe Doe", 1000);
+		assertEquals(1000, sumReturnedToTheCustomer);
+		assertEquals(4000, testCOmpany.getSumToOperate());
+	}
+
+
+
+}

4714508f7ace1312022bbfb75c5b2903bc1d1f7b
Oleksandr Ryzhkov <oryzhkov@paymentus.com>
2019-09-30 06:54:29 -0400

    Exception examples1 - handling checked and unchecked exceptions

--- src/com/go2it/edu/exceptions/ClassToThrowException.java
+++ src/com/go2it/edu/exceptions/ClassToThrowException.java
@@ -1,5 +1,7 @@
 package com.go2it.edu.exceptions;
 
+import java.io.FileNotFoundException;
+
 /**
  * @author Alex Ryzhkov
  */
@@ -14,9 +16,10 @@ public class ClassToThrowException {
 		return title;
 	}
 
-	public void setTitle(String title) {
+	public void setTitle(String title) throws FileNotFoundException {
 		if (title == null || title.isEmpty()) {
-			throw new IllegalArgumentException("The title was empty, couldn't set it up");
+//			throw new IllegalArgumentException("The title was empty, couldn't set it up");
+			throw new FileNotFoundException("Some random exception");
 		}
 		this.title = title;
 	}
--- src/com/go2it/edu/exceptions/ExceptionExample.java
+++ src/com/go2it/edu/exceptions/ExceptionExample.java
@@ -1,14 +1,35 @@
 package com.go2it.edu.exceptions;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+
 /**
  * @author Alex Ryzhkov
  */
 public class ExceptionExample {
 
-	public static void main(String[] args){
-		ClassToThrowException testClass = new ClassToThrowException("testTitle");
-		testClass.setTitle(null);
+	public static void main(String[] args) {
+		try {
+			try {
+				ClassToThrowException testClass = new ClassToThrowException("testTitle");
+				testClass.setTitle(null);
 
-		System.out.println("It will not be executed");
+				System.out.println("It will not be executed");
+			} catch (IllegalArgumentException e) {
+				System.out.println("Oops! Exception was thrown: " + e.getMessage());
+			} catch (FileNotFoundException e) {
+				System.out.println("It was FileNotFoundException!");
+			} catch (IOException e) {
+				System.out.println("Some log");
+			} catch (Exception e) {
+				//exception logging
+			} finally {
+				System.out.println("This line will be shown");
+			}
+		} catch (Exception e) {
+			System.out.println(Arrays.toString(e.getSuppressed()));
+		}
+		System.out.println("After try-catch-finally");
 	}
 }
